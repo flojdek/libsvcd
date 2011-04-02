e1e7896c4a5928a2248cfacac20b8d158c8719ee
Tomasz Kupczyk <kupczyk.tomasz@gmail.com>
2011-04-02 22:40:05 +0200

    Tweak test.

--- tests/payloadsizetest.cpp
+++ tests/payloadsizetest.cpp
@@ -6,6 +6,8 @@ int main()
 {
 	NodesSearch nodes_finder;
 
+	// Too big payload.
+
 	char* payload = new (std::nothrow) char [nodes_finder.GetMaxMessageSize() + 1];
 	assert(payload);
 	memset(payload, 1, nodes_finder.GetMaxMessageSize() + 1);
@@ -20,6 +22,21 @@ int main()
 		was_exception = true;
 	}
 	assert(was_exception);
+
+	assert(std::string(nodes_finder.GetPayload()) == "");
+
+	// Exact payload.
+
+	payload[nodes_finder.GetMaxMessageSize()] = 0;
+
+	try
+	{
+		nodes_finder.SetPayload(payload);
+	}
+	catch (std::length_error& ex)
+	{
+		assert(false);
+	}
 
 	return 0;
 }
